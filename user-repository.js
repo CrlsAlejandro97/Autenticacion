8845508c02866919e88dfcaed93253e02f7e054f
CrlsAlejandro97 <aleocampoiza97@gmail.com>
2026-01-14 23:32:57 +0000

    FC

---
+++ user-repository.js
@@ -0,0 +1,58 @@
+import DBLocal from 'db-local'
+import bcrypt from 'bcrypt'
+import { SALT_ROUNDS } from './config.js'
+
+const { Schema } = new DBLocal({path: './db'})
+
+const User = Schema('User',{
+    _id: {type: String, required: true},
+    username: {type: String, required: true},
+    password: {type: String, required: true}
+    })
+
+export class UserRepository{
+    static async create ({username, password}){
+        // 1. Validaciones de username (opcional: usar zod)
+        Validation.username(username)
+        Validation.password(password)
+
+        // 2. Asegurarse que username no existe
+        const user = User.findOne({username})
+        if (user) throw new Error('username already exists')
+
+        const id = crypto.randomUUID()
+        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+
+        User.create({
+            _id: id,
+            username,
+            password:hashedPassword
+        }).save()
+
+        return id
+    }
+    static async login ({username, password}){
+        Validation.username(username)
+        Validation.password(password)
+        const user = User.findOne({ username })
+        if (!user) throw new Error('username does not exist')
+
+        const isValid = await bcrypt.compareSync(password, user.password)
+        if (!isValid) throw new Error('password is invalid')
+        
+        const {password: _, ... publicUser} = user // para no devolver password en la response
+        return publicUser
+    }
+}
+
+class Validation {
+    static username(username){
+        if (typeof username !== 'string') throw new Error('username must be a string')
+        if (username.length < 3) throw new Error('username must be at least 3 characters log')
+    }
+
+    static password(password){
+        if (typeof password !== 'string') throw new Error('password must be a string')
+        if (password.length < 6) throw new Error('password must be at least 6 characters log')
+    }
+}
