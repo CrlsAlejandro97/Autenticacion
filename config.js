c051071d634b2c274f700f298c94a2720628eae9
CrlsAlejandro97 <aleocampoiza97@gmail.com>
2026-01-16 21:12:27 +0000

    JWT implement

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export const {
     PORT = 3000,
-    SALT_ROUNDS = 10
+    SALT_ROUNDS = 10,
+    SECRET_JWT_KEY = 'J5s1Ed2sHBxaehspqKoVX66oj'
 } = process.env
 
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
+import jwt from 'jsonwebtoken'
 import { PORT } from "./config.js";
+import { SECRET_JWT_KEY } from "./config.js";
 import { UserRepository } from "./user-repository.js";
+import cookieParser from "cookie-parser";
+import { use } from "bcrypt/promises.js";
 
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.json())
+app.use(cookieParser())
 
 
 app.get('/', (req, res) => {
@@ -16,7 +21,20 @@ app.post('/login', async(req, res) => {
     console.log(req.body)
     try{
         const user = await UserRepository.login({username,password})
-        res.send({user})
+        const token = jwt.sign(
+            {id: user._id, username: user.username}, 
+            SECRET_JWT_KEY,
+            {
+                expiresIn: '1h'
+            })
+        res
+        .cookie('access_token',token,{
+            httpOnly: true, //la cookie solo se puede acceder desde el servidor
+            secure: process.env.NODE_ENV == 'production', // la cookie solo se puede acceder en https
+            sameSite: 'strict', // Solo se puede acceder desde el mismo dominio
+            maxAge: 100 * 60 * 60 // la cookie solo tiene validez de 1 hora
+        })
+        .send({user,token})
     } catch (error){
         res.status(401).send(error.message)
     }
